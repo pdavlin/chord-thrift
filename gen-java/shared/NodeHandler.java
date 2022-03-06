fc9fdd2d450015bc046930d271621d46294be4e1
pdavlin <patrick@davlin.io>
2022-03-06 15:26:04 -0600

    Lots of cleanup

--- gen-java/shared/NodeHandler.java
+++ gen-java/shared/NodeHandler.java
@@ -1,32 +1,16 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import org.apache.thrift.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.thrift.TApplicationException;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
-import java.io.UnsupportedEncodingException;
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TServer.Args;
-import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.server.TThreadPoolServer;
-import org.apache.thrift.transport.TSSLTransportFactory;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
-import org.apache.thrift.transport.*;
-import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
 import org.slf4j.helpers.MessageFormatter;
-import org.apache.thrift.TException;
-import org.apache.thrift.*;
-import java.util.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.io.UnsupportedEncodingException;
-import java.lang.Math;
 
 public class NodeHandler implements Node.Iface {
 
@@ -70,7 +54,18 @@ public class NodeHandler implements Node.Iface {
 
 		// ask supernode about who to talk to to get successor/predecessor
 		// client.getNode();
-		NodeData nodeData = client.join(currentNode.port);
+		// NodeData nodeData = client.join(currentNode.port);
+		NodeData nodeData = new NodeData();
+		try {
+			nodeData = client.join(currentNode.port);
+		} catch (TApplicationException tae) {
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException ie) {
+				nodeLog("Interrupted");
+			}
+			nodeData = client.join(currentNode.port);
+		}
 		if (nodeData.port != 0) {
 			// nodes.add(nodeData);
 			// findSuccessor(currentNode.id);
@@ -89,12 +84,9 @@ public class NodeHandler implements Node.Iface {
 			nodeLog("Calling client.findPred");
 			// NodeData predNode = nodeClient.findPred(key);
 			predNode = findPred(key);
-			// nodeLog("Key: " + Integer.toString(key) + " <> Successor: " +
-			// Integer.toString(n.id));
+
 			nodeLog("Key {} successor: {}", key, n.id);
 			nodeLog("Key {} successor: {}", key, predNode.id);
-			// nodeLog("Key: " + Integer.toString(key) + " <> Predecessor: " +
-			// Integer.toString(predNode.id));
 
 			TTransport predTransport = new TSocket("localhost", predNode.port);
 			TProtocol predProtocol = new TBinaryProtocol(predTransport);
@@ -119,11 +111,8 @@ public class NodeHandler implements Node.Iface {
 				key = key % 16;
 				currentNode.finger = key;
 				nodes.add(currentNode);
-				// nodeLog(Integer.toString(i) + ": " + Integer.toString(key) + " <> Successor:
-				// " + Integer.toString(currentNode.id));
 				nodeLog("Key {} successor: {}", key, currentNode.id);
 			}
-			// nodeLog("New Node finger table size: " + Integer.toString(nodes.size()));
 			nodeLog("New Node finger table size: {}", nodes.size());
 			client.postJoin(currentNode.port);
 		}
@@ -136,23 +125,16 @@ public class NodeHandler implements Node.Iface {
 
 	@Override
 	public java.lang.String get(java.lang.String book_title) throws org.apache.thrift.TException {
-		// nodeLog("Node:" + Integer.toString(currentNode.port) + " attempting get() for
-		// book " + book_title);
 		nodeLog("Node: {} attempting to get() for book {}", currentNode.port, book_title);
-		// nodeLog("book_title hash: " + Integer.toString(getHashKey(book_title)));
 		nodeLog("Title hash: {}", getHashKey(book_title));
 		return "test";
 	}
 
 	@Override
 	public void updateDHT() throws org.apache.thrift.TException {
-		// nodeLog("updating DHT for node ID:Port -> " +
-		// Integer.toString(currentNode.id) + ":" + Integer.toString(currentNode.port));
 		nodeLog("Updating DHT for {}:{}", currentNode.id, currentNode.port);
-		// nodeLog("Current Finger Table size: " + Integer.toString(nodes.size()));
 		nodeLog("Current Finger Table size: {}", nodes.size());
 		for (int i = 1; i < 4; i++) {
-			// nodeLog("Trying to get fingertable node " + Integer.toString(i));
 			nodeLog("Trying to get fingertable node {}", i);
 			// NodeData fingerNode = nodes.get(i);
 			int key = currentNode.id + (int) (Math.pow(2, i));
@@ -164,18 +146,16 @@ public class NodeHandler implements Node.Iface {
 			} catch (IndexOutOfBoundsException e) {
 				nodes.add(n);
 			}
-			// nodeLog("Key: " + Integer.toString(key) + " <> Successor: " +
-			// Integer.toString(n.id));
 			nodeLog("Key {} successor: {}", key, n.id);
 		}
 	}
 
 	@Override
 	public NodeData findSuccessor(int key) throws TException {
-		nodeLog("Finding Successor for key: " + Integer.toString(key));
+		nodeLog("Finding successor for key {}", key);
 		// Key found
 		if (key == currentNode.id) {
-			nodeLog("Current node matches key: " + Integer.toString(key));
+			nodeLog("Current node matches key {}", key);
 			return currentNode;
 		}
 		// Only node in cluster will hold all keys
@@ -189,12 +169,6 @@ public class NodeHandler implements Node.Iface {
 		if (predecessor.id == currentNode.id) {
 			return nodes.get(0);
 		}
-		/*
-		 * NodeData succ = nodes.get(0);
-		 * if (predecessor.id == succ.id) {
-		 * return succ;
-		 * }
-		 */
 		TTransport transport = new TSocket("localhost", predecessor.port);
 		TProtocol protocol = new TBinaryProtocol(transport);
 		Node.Client client = new Node.Client(protocol);
@@ -212,38 +186,11 @@ public class NodeHandler implements Node.Iface {
 
 	@Override
 	public NodeData findPred(int key) {
-		// nodeLog("Finding predecessor for key " + Integer.toString(key));
 		nodeLog("Finding predecessor for key {}", key);
 		// Compare node to successor
-		// nodeLog("Current Node: " + Integer.toString(currentNode.id));
 		NodeData tempNode = nodes.get(0);
 		// TODO: HOW DO I DO THIS LOGIC CORRECTLY I AM LOCKING MY STUFFFFFFFFFFFF
-		// predNode being returned as currentNode incorrectly?
-		// nodeLog("successor ID: " + Integer.toString(tempNode.id));
-		/*
-		 * if (currentNode.id > tempNode.id) {
-		 * nodeLog("currentNode.id > tempNode.id");
-		 * if (key <= tempNode.id || key >= currentNode.id) {
-		 * nodeLog("key <= tempNode.id || key >= currentNode.id");
-		 * return currentNode;
-		 * }
-		 * else {
-		 * tempNode = findClosestPrecedingFinger(key);
-		 * return tempNode;
-		 * }
-		 * }
-		 * else {
-		 * nodeLog("currentNode.id <= tempNode.id");
-		 * if (key > currentNode.id || key <= tempNode.id) {
-		 * nodeLog("key > currentNode.id || key <= tempNode.id");
-		 * return currentNode;
-		 * }
-		 * else {
-		 * tempNode = findClosestPrecedingFinger(key);
-		 * return tempNode;
-		 * }
-		 * }
-		 */
+
 		if (currentNode.id < tempNode.id) {
 			if (key > currentNode.id && key <= tempNode.id) {
 				nodeLog("Predecessor for key: {}", currentNode.id);
@@ -251,29 +198,18 @@ public class NodeHandler implements Node.Iface {
 			}
 		}
 		if (currentNode.id > tempNode.id) {
-			// if (key > currentNode.id && key <= tempNode.id) {
-			nodeLog("Predecessor for key: {}", currentNode.id);
+			if (currentNode.id < key && key >= tempNode.id) {
+				nodeLog("Predecessor for key: {}", currentNode.id);
+			}
 			return currentNode;
-
-			/*
-			 * if (key <= tempNode.id && key > currentNode.id) {
-			 * nodeLog("Making call to FCPF");
-			 * tempNode = findClosestPrecedingFinger(key);
-			 * nodeLog("FCPF current node: " + Integer.toString(currentNode.id));
-			 * nodeLog("FCPF node id found: " + Integer.toString(tempNode.id));
-			 */
-			// }
 		}
-		// nodeLog("Found predecessor for key +" + Integer.toString(tempNode.id) + ": "
-		// + Integer.toString(tempNode.id));
+
 		nodeLog("Predecessor for key: {}", tempNode.id);
 		return tempNode;
 	}
 
 	@Override
 	public NodeData findClosestPrecedingFinger(int key) {
-		// nodeLog("Finding closest finger for key: " + Integer.toString(key) + " @ node
-		// ID: " + Integer.toString(currentNode.id));
 		nodeLog("Finding closest finger for key {} at node ID {}", key, currentNode.id);
 		for (int i = nodes.size() - 1; i >= 0; i--) {
 			NodeData f = nodes.get(i);
@@ -286,19 +222,12 @@ public class NodeHandler implements Node.Iface {
 
 	@Override
 	public NodeData getNodeSuccessor() {
-		// nodeLog("getNodeSuccessor reached @ node " + Integer.toString(currentNode.id)
-		// + ":" + Integer.toString(currentNode.port));
 		nodeLog("getNodeSuccessor reached at node {}: {}", currentNode.id, currentNode.port);
 		if (nodes.size() > 0) {
 			NodeData s = nodes.get(0);
-			// nodeLog("getNodeSuccessor: " + Integer.toString(currentNode.id) + ":" +
-			// Integer.toString(currentNode.port) + " -> " + Integer.toString(s.id) + ":" +
-			// Integer.toString(s.port));
 			nodeLog("getNodeSuccessor: {}:{} -> {}:{}", currentNode.id, currentNode.port, s.id, s.port);
 			return s;
 		} else {
-			// nodeLog("No successor node @ node:Port -> " +
-			// Integer.toString(currentNode.id) + ":" + Integer.toString(currentNode.port));
 			nodeLog("No succesor node at {}:{}", currentNode.id, currentNode.port);
 			return null;
 		}
@@ -308,15 +237,12 @@ public class NodeHandler implements Node.Iface {
 	public void setNodeSuccessor(NodeData successor) {
 		if (nodes.size() > 0) {
 			nodes.set(0, successor);
-			// nodeLog("Node " + Integer.toString(currentNode.id) + " Successor updated to "
-			// + Integer.toString(successor.id));
 			nodeLog("Node {} successor updated to {}", successor.id);
 		}
 	}
 
 	public static int getHashKey(String input) {
 		try {
-			// nodeLog("Attempting to get hash for string: " + input);
 			nodeLog("Attempting to get hash for string: {}", input);
 			MessageDigest md = MessageDigest.getInstance("SHA-1");
 			byte[] encoded = md.digest(input.getBytes("UTF-8"));
@@ -345,7 +271,7 @@ public class NodeHandler implements Node.Iface {
 	}
 
 	public static void nodeLog(String msg, Object... objects) {
-		String prepend = loggingNodeId != null ?"N" + loggingNodeId + ":  " : "NEW: ";
+		String prepend = loggingNodeId != null ? "N" + loggingNodeId + ":  " : "NEW: ";
 		System.out.println(prepend + MessageFormatter.arrayFormat(msg, objects).getMessage());
 	}
 
